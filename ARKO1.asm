e9b07000d7f4f794a287ed2241ed14e033811dc5
Maja <maja.jablonska.yyy@gmail.com>
2018-04-01 21:32:20 +0200

    ...

--- ARKO1.asm
+++ ARKO1.asm
@@ -10,7 +10,7 @@
 	prompt1: .asciiz "of the 1st triangle.\n"
 	prompt2: .asciiz "of the 2nd triangle.\n"
 	
-	promptx: .asciiz "Enter x coordinate: "
+	promptc: .asciiz "Enter coordinate:\n"
 	prompty: .asciiz "Enter y coordinate: "
 	promptz: .asciiz "Enter z coordinate: "
 	newline: .asciiz "\n"
@@ -22,13 +22,29 @@
 	maskx: .word 536346624
 	maskcolor: .word 2147483648
 	
-	.eqv A1 $s0
-	.eqv B1 $s1
-	.eqv C1 $s2
+	.eqv A1.x -72($sp)
+	.eqv A1.y -68($sp)
+	.eqv A1.z -64($sp)
 	
-	.eqv A2 $s3
-	.eqv B2 $s4
-	.eqv C2 $s5
+	.eqv B1.x -60($sp)
+	.eqv B1.y -56($sp)
+	.eqv B1.z -52($sp)
+	
+	.eqv C1.x -48($sp)
+	.eqv C1.y -44($sp)
+	.eqv C1.z -40($sp)
+	
+	.eqv A2.x -36($sp)
+	.eqv A2.y -32($sp)
+	.eqv A2.z -28($sp)
+	
+	.eqv B2.x -24($sp)
+	.eqv B2.y -20($sp)
+	.eqv B2.z -16($sp)
+	
+	.eqv C2.x -12($sp)
+	.eqv C2.y -8($sp)
+	.eqv C2.z -4($sp)
 	
 	.eqv BMPH $s6
 	.eqv BMP $s7
@@ -36,12 +52,12 @@
 	.eqv WHITE 0xFFFFFF
 	.eqv BLACK 0x000000
 	
-	.eqv WIDTH $t0
-	.eqv HEIGHT $t1
-	.eqv BITS_PP $t3
-	.eqv PIXELS_ROW $t4
-	.eqv PADDING $t5
-	.eqv BMP_BUFFER $t6
+	.eqv WIDTH $s0
+	.eqv HEIGHT $s1
+	.eqv BITS_PP $s2
+	.eqv PIXELS_ROW $s3
+	.eqv PADDING $s4
+	.eqv BMP_BUFFER $s5
 	
 	bmp_path: .asciiz "in.bmp"
 
@@ -78,230 +94,24 @@ close_bmp:
 	move $a0, BMPH
 	syscall
 	
-
-input_A1:
-	li $v0, 4	#print instructions
-	la $a0, promptA
-	syscall 
-	li $v0, 4	
-	la $a0, prompt1
-	syscall 
+	li $t0, 18
 	
-	#addi A1, A1, 2147483648
-	
-	li $v0, 4	#print string: ask for x coordinate
-	la $a0, promptx
+get_coord_input:
+	li $v0, 4	#ask for coordinate
+	la $a0, promptc
 	syscall
 	
-	li $v0, 5	#read the x coordinate as integer
-	syscall
-	sll $t0, $v0, 20	#shift the x on the right position
-	add A1, A1, $t0		#place the shifted x value in the correct register
-	
-	li $v0, 4	#print string: ask for y coordinate
-	la $a0, prompty
+	li $v0, 5	#read the coordinate as integer
 	syscall
 	
-	li $v0, 5	#read the y coordinate as integer
-	syscall
-	sll $t0, $v0, 9		#shift the y on the right position
-	add A1, A1, $t0		#place the shifted y value in the correct register	
 	
-	li $v0, 4	#print string: ask for z coordinate
-	la $a0, promptz
-	syscall
+	sw $v0, 0($sp)	#push the coordinate on stack
+	addi $sp, $sp, 4
 	
-	li $v0, 5	#read the z coordinate as integer
-	syscall
-	addi $t0, $v0, 0	#shift the z on the right position
-	add A1, A1, $t0		#place the shifted z value in the correct register
+	addi $t0, $t0, -1 #decrement the counter
 	
-input_B1:
-	li $v0, 4	#print instructions
-	la $a0, promptB
-	syscall 
-	li $v0, 4	
-	la $a0, prompt1
-	syscall 
+	bnez $t0, get_coord_input
 	
-	#addi A1, A1, 2147483648
-	
-	li $v0, 4	#print string: ask for x coordinate
-	la $a0, promptx
-	syscall
-	
-	li $v0, 5	#read the x coordinate as integer
-	syscall
-	sll $t0, $v0, 20	#shift the x on the right position
-	add B1, B1, $t0		#place the shifted x value in the correct register
-	
-	li $v0, 4	#print string: ask for y coordinate
-	la $a0, prompty
-	syscall
-	
-	li $v0, 5	#read the y coordinate as integer
-	syscall
-	sll $t0, $v0, 9		#shift the y on the right position
-	add B1, B1, $t0		#place the shifted y value in the correct register	
-	
-	li $v0, 4	#print string: ask for z coordinate
-	la $a0, promptz
-	syscall
-	
-	li $v0, 5	#read the z coordinate as integer
-	syscall
-	addi $t0, $v0, 0	#shift the z on the right position
-	add B1, B1, $t0		#place the shifted z value in the correct register
-	
-input_C1:
-	li $v0, 4	#print instructions
-	la $a0, promptC
-	syscall 
-	li $v0, 4	
-	la $a0, prompt1
-	syscall 
-	
-	#addi A1, A1, 2147483648
-	
-	li $v0, 4	#print string: ask for x coordinate
-	la $a0, promptx
-	syscall
-	
-	li $v0, 5	#read the x coordinate as integer
-	syscall
-	sll $t0, $v0, 20	#shift the x on the right position
-	add C1, C1, $t0		#place the shifted x value in the correct register
-	
-	li $v0, 4	#print string: ask for y coordinate
-	la $a0, prompty
-	syscall
-	
-	li $v0, 5	#read the y coordinate as integer
-	syscall
-	sll $t0, $v0, 9		#shift the y on the right position
-	add C1, C1, $t0		#place the shifted y value in the correct register	
-	
-	li $v0, 4	#print string: ask for z coordinate
-	la $a0, promptz
-	syscall
-	
-	li $v0, 5	#read the z coordinate as integer
-	syscall
-	addi $t0, $v0, 0	#shift the z on the right position
-	add C1, C1, $t0		#place the shifted z value in the correct register
-	
-	
-	
-input_A2:
-	li $v0, 4	#print instructions
-	la $a0, promptA
-	syscall 
-	li $v0, 4	
-	la $a0, prompt2
-	syscall 
-	
-	#addi A1, A1, 2147483648
-	
-	li $v0, 4	#print string: ask for x coordinate
-	la $a0, promptx
-	syscall
-	
-	li $v0, 5	#read the x coordinate as integer
-	syscall
-	sll $t0, $v0, 20	#shift the x on the right position
-	add A2, A2, $t0		#place the shifted x value in the correct register
-	
-	li $v0, 4	#print string: ask for y coordinate
-	la $a0, prompty
-	syscall
-	
-	li $v0, 5	#read the y coordinate as integer
-	syscall
-	sll $t0, $v0, 9		#shift the y on the right position
-	add A2, A2, $t0		#place the shifted y value in the correct register	
-	
-	li $v0, 4	#print string: ask for z coordinate
-	la $a0, promptz
-	syscall
-	
-	li $v0, 5	#read the z coordinate as integer
-	syscall
-	addi $t0, $v0, 0	#shift the z on the right position
-	add A2, A2, $t0		#place the shifted z value in the correct register
-	
-input_B2:
-	li $v0, 4	#print instructions
-	la $a0, promptB
-	syscall 
-	li $v0, 4	
-	la $a0, prompt2
-	syscall 
-	
-	#addi A1, A1, 2147483648
-	
-	li $v0, 4	#print string: ask for x coordinate
-	la $a0, promptx
-	syscall
-	
-	li $v0, 5	#read the x coordinate as integer
-	syscall
-	sll $t0, $v0, 20	#shift the x on the right position
-	add B2, B2, $t0		#place the shifted x value in the correct register
-	
-	li $v0, 4	#print string: ask for y coordinate
-	la $a0, prompty
-	syscall
-	
-	li $v0, 5	#read the y coordinate as integer
-	syscall
-	sll $t0, $v0, 9		#shift the y on the right position
-	add B2, B2, $t0		#place the shifted y value in the correct register	
-	
-	li $v0, 4	#print string: ask for z coordinate
-	la $a0, promptz
-	syscall
-	
-	li $v0, 5	#read the z coordinate as integer
-	syscall
-	addi $t0, $v0, 0	#shift the z on the right position
-	add B2, B2, $t0		#place the shifted z value in the correct register
-	
-input_C2:
-	li $v0, 4	#print instructions
-	la $a0, promptC
-	syscall 
-	li $v0, 4	
-	la $a0, prompt2
-	syscall 
-	
-	#addi A1, A1, 2147483648
-	
-	li $v0, 4	#print string: ask for x coordinate
-	la $a0, promptx
-	syscall
-	
-	li $v0, 5	#read the x coordinate as integer
-	syscall
-	sll $t0, $v0, 20	#shift the x on the right position
-	add C2, C2, $t0		#place the shifted x value in the correct register
-	
-	li $v0, 4	#print string: ask for y coordinate
-	la $a0, prompty
-	syscall
-	
-	li $v0, 5	#read the y coordinate as integer
-	syscall
-	sll $t0, $v0, 9		#shift the y on the right position
-	add C2, C2, $t0		#place the shifted y value in the correct register	
-	
-	li $v0, 4	#print string: ask for z coordinate
-	la $a0, promptz
-	syscall
-	
-	li $v0, 5	#read the z coordinate as integer
-	syscall
-	addi $t0, $v0, 0	#shift the z on the right position
-	add C2, C2, $t0		#place the shifted z value in the correct register
 	
 calculate_sizes:
 	ulw WIDTH, 18(BMP) #read the bmp width
@@ -328,20 +138,84 @@ calculate_sizes:
 	
 #TEST: draw one triangle
 iterate:
+	
 	la BMP_BUFFER, (BMP)
-	ulh $t7, 10(BMP_BUFFER)
-	add BMP_BUFFER, BMP_BUFFER, $t7
-	addi BMP_BUFFER, BMP_BUFFER, -1
-	ulh $t6, (BMP_BUFFER)
+	ulh $t1, 10(BMP_BUFFER)
+	add BMP_BUFFER, BMP_BUFFER, $t1
+	ulh $t0, (BMP_BUFFER)	#$t0 has the beginning of pixel array
 	
-#$a0, $a1, $a2- function arguments (type: vertices)
-#result in $v0
-#return (p1.x - p3.x) * (p2.y - p3.y) - (p2.x - p3.x) * (p1.y - p3.y);
-calc_sign:
+#test: draw one triangle (1st one)
 	
+
+
 	
 
 exit:
 	li $v0, 10
 	syscall
 
+
+#$a0, $a1, $a2- x function arguments (type: vertices)
+#$t1, $t2, $t3- y function arguments
+#$a0, $a1- x and y of given point
+#result in $v0
+#$v0- result (1- true, 0 otherwise)
+is_in_t1:
+	
+	sw $ra, 0($sp)
+	addi $sp, $sp, 4
+	#Calc sign for A1 and B1
+	move $t0, $a1
+	lw $a1, A1.x
+	lw $a2, B1.x
+	lw $t1, A1.y
+	lw $t2, B1.y
+	jal calc_sign
+	move $t9, $v0
+	
+	#Calc sign for A1 and C1
+	lw $a1, A1.x
+	lw $a2, C1.x
+	lw $t1, A1.y
+	lw $t2, C1.y
+	jal calc_sign
+	move $t8, $v0
+	
+	#Calc sign for B1 and C1
+	lw $a1, B1.x
+	lw $a2, C1.x
+	lw $t1, B1.y
+	lw $t2, C1.y
+	jal calc_sign
+	move $t7, $v0
+	
+	#1 z 3 i 2 z 3
+	mul $t9, $t9, $t7
+	mul $t8, $t8, $t7
+	mul $t7, $t8, $t9
+	
+	lw $ra, -4($sp)
+	addi $sp, $sp, -4
+	
+	bgez g_t_z
+		li $v0, 0
+		jr $ra
+	g_t_z:
+		li $v0, 1
+		jr $ra
+
+#TOCONSIDER: store both values as halfwords?
+
+#return (p1.x - p3.x) * (p2.y - p3.y) - (p2.x - p3.x) * (p1.y - p3.y);
+calc_sign:
+	sub $t4, $a0, $a2
+	sub $t5, $t2, $t3
+	mul $t4, $t4, $t5
+	
+	sub $t5, $a1, $a2
+	sub $t6, $t1, $t3
+	mul $t5, $t5, $t6
+	
+	sub $v0, $t4, $t5
+	
+	jr $ra`
---
+++ vertices on stack README.txt
@@ -0,0 +1,22 @@
+Vertices will be held on stack, where:
+There are two triangles, each with vertices A, B, C, each with coordinates x, y, z
+4*18 = 72 bytes
+
+-72($SP) A1.x
+-68($SP) A1.y
+-64($SP) A1.z
+-60($SP) B1.x
+-56($SP) B1.y
+-52($SP) B1.z
+-48($SP) C1.x
+-44($SP) C1.y
+-40($SP) C1.z
+-36($SP) A2.x
+-32($SP) A2.y
+-28($SP) A2.z
+-24($SP) B2.x
+-20($SP) B2.y
+-16($SP) B2.z
+-12($SP) C2.x
+-8($SP) C2.y
+-4($SP) C2.z
